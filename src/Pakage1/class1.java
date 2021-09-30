a133c9e93c12c75b5832c0b979ddaeaecd57ec49
pradeep-git135 <pradeepcr135@gmail.com>
2021-09-30 13:38:53 +0530

    git commands 2

--- src/Pakage1/class1.java
+++ src/Pakage1/class1.java
@@ -1,5 +1,67 @@
-package Pakage1;
-
-public class class1 {
-
-}
+//package Pakage1;
+//
+//public class class1 {
+//
+//
+//push code
+//
+//Connection to remote url:-
+//git remote add origin giturl
+//
+//get project status
+//git status
+//
+//To add files from unstaged to staged area
+//git add <filename>
+//
+//Write commit message
+//git commit -m "message"
+//
+//To push code
+//git push origin <branchName>
+//
+//
+//To pull the code
+//git pull origin <branchName>
+//
+//To clone remote repository
+//git clone giturl
+//
+//Branching:-
+//
+//To create a new branch
+//git branch <branchName>
+//
+//Switch to branch
+//git checkout <branchName>
+//
+//To List all branches:-
+//git branch
+//
+//Delete branch
+//git branch -d <branchName>
+//
+//To remove file
+//git rm -f -r <fileName>
+//
+//To check the difference b/w 2 branch
+//git diff <branchName>
+//
+//To merge
+//git merge <branchName>
+//
+//To reset the commit
+//git reset <commitId>
+//
+//To get git commit logs:
+//git log
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//}
